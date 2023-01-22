0363399941391eb811f11c03e5642245aedc0aea
System Administrator <root@eduroam-83-148.zdv.Uni-Mainz.DE>
2023-01-22 14:27:34 +0100

    test

--- RDFObjects.cxx
+++
@@ -1,9 +0,0 @@
-#include "RDFObjects.h"
-
-const std::string TruthParticle::PREFIX = "mc";
-const std::string Electron::PREFIX = "electron";
-const std::string Photon::PREFIX = "photon";
-const std::string Muon::PREFIX = "muon";
-const std::string Track::PREFIX = "track";
-const std::string Cluster::PREFIX = "cluster";
-
